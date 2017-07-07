e6be618a2b1ef3c24645d7a82ce95007934cdcbc
Ravinder Soni <sonirav@gmail.com>
2017-07-07 13:27:31 -0700

    update4

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var mysql = require("mysql");
+//var mysql = require("mysql");
 var path = require("path");
 //var fs = require("fs");
 //var methodOverride = require("method-override");
@@ -42,55 +42,6 @@ app.get('/mresume',function(req,res){
 app.get('/contactm',function(req,res){
 	res.sendFile(path.join(__dirname,"/assets/html/contactme.html"))
 });
-
-
-
-
-//app.post("/",function(req,res){
-  //console.log(req.body);
-  //console.log(req.body.name);
-   // console.log(req.body.address);
-//    var fld1=req.body.name;
- //   var fld2=req.body.address;
-//connection.query('INSERT INTO mytbl SET ?',{fname:fld1,address:fld2},function(err,res){
-// if (err) throw err;
- //     console.log('Data Inserted to database ');
- //});
-//});
-/*
-function makeConnection() {
-    connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: 'root',
-        password: 'ravi1962',
-        database: 'mydb_1'
-
-    });
-}
-makeConnection();
-
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-// Express and MySQL code should go here.
-function showdata()
-{
-connection.query("SELECT * FROM mytbl LIMIT 10;", function(err,res)
-{
- if (err) throw err;
-      console.log('Data Inserted to database ');
-}
-})
-
-*/
-
 
 app.listen(PORT, function() {
   console.log("Listening on PORT " + PORT);
